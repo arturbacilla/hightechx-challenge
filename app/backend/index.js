182c947a56ebe1553ccb9fba9911f5729d70ad3d
arturbacilla <artur.bacilla1@gmail.com>
2022-08-15 12:16:37 -0300

    Adding upstream

--- app/backend/index.js
+++ app/backend/index.js
@@ -5,9 +5,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.listen(3001, () => console.log('Backend ouvindo na porta 3001!'));
+app.listen(3001, () => console.log('Backend com CORS ativado, ouvindo na porta 3001!'));
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
-  response.send('It is working my friend');
+  response.send('It is working my friend!');
 });
