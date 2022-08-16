5d8b5661ad259c73fc276771347e7bb6e5c2b17c
arturbacilla <artur.bacilla1@gmail.com>
2022-08-16 16:07:53 -0300

    :wrench: config: Setup part 3

--- app/backend/index.js
+++ app/backend/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.listen(3001, () => console.log('Backend com CORS ativado, ouvindo na porta 3001!'));
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => console.log(`Backend com CORS ativado, ouvindo na porta ${PORT}!`));
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
